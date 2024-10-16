bf25e3f32ed86fe1dadc252b749c0e311f8a0004
Thiago <thiagoleonardo1995@live.com>
2024-10-16 17:31:36 -0300

    Fragments

--- Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.fragments
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.fragments.fragments.ChamadasFragment
+import com.example.fragments.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas : Button
+    private lateinit var btnChamadas : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +23,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        btnConversas = findViewById(R.id.btn_conversas)
+        btnChamadas = findViewById(R.id.btn_chamadas)
+
+        btnConversas.setOnClickListener {
+            /* // Modo Comum
+            // beginTransaction() = Iniciar Transação
+            val fragmentManager = supportFragmentManager.beginTransaction()
+
+            // Realizar alterações necessárias
+            fragmentManager.add(R.id.fragment_conteudo, ConversasFragment())
+
+            // Salvar as alterações
+            fragmentManager.commit()
+             */
+
+            // Modo Simplificado
+            supportFragmentManager.beginTransaction()
+                // add adiciona um sobre o outro, por isso usa-se replace
+                // .add(R.id.fragment_conteudo, ConversasFragment())
+                .replace(R.id.fragment_conteudo, ConversasFragment())
+                .commit()
+
+        }
+
+        btnChamadas.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_conteudo, ChamadasFragment())
+                .commit()
+        }
+
+        // Remover um Fragment exemplo:
+        /* val conversasFragment = ConversasFragment()
+        btnChamadas.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .remove(conversasFragment)
+                .commit()
+        } */
     }
 }
